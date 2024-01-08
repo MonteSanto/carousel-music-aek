26ce496570d1f0a00e538e6db28a5e7ea3de5636
Alexis Monte-Santo <alexms91@hotmail.com>
2024-01-08 09:29:20 +0200

    real music

--- src/variable.js
+++ src/variable.js
@@ -1,12 +1,12 @@
 const exhibit_0  = {path: "chapter", id:1, titleGR: "Music", titleEN:" ", date: "", descriptionGR: "", sound: ""}
 
-const exhibit_1  = {path: "images/1.png", id:2, titleGR: "Adagio", titleEN:" ", date: "", descriptionGR: "", sound: "track_1.wav"}
-const exhibit_2  = {path: "images/2.png", id:3, titleGR: "Adinamia", titleEN:" ", date: "", descriptionGR: "", sound: "track_2.wav"}
+const exhibit_1  = {path: "images/1.png", id:2, titleGR: "Adagio", titleEN:" ", date: "", descriptionGR: "", sound: "AEKARA_1968_Theod.Agriniotis copy.wav"}
+const exhibit_2  = {path: "images/2.png", id:3, titleGR: "Adinamia", titleEN:" ", date: "", descriptionGR: "", sound: "AEKAΡΑ 1968 copy.wav"}
 
-const exhibit_3  = {path: "images/3.png", id:4, titleGR: "asterias", titleEN:" ", date: "", descriptionGR: "", sound: ""}
-const exhibit_4  = {path: "images/4.png", id:5, titleGR: "astron", titleEN:" ", date: "", descriptionGR: "", sound: ""}
+const exhibit_3  = {path: "images/3.png", id:4, titleGR: "asterias", titleEN:" ", date: "", descriptionGR: "", sound: "KC 246 ΑΕΚ ( Μύτικα - Σπ. Μυροφτας) Σπ. Μαυροφτας ΝΕΑΡΧΟΣ copy.mp3"}
+const exhibit_4  = {path: "images/4.png", id:5, titleGR: "astron", titleEN:" ", date: "", descriptionGR: "", sound: "Α.Ε.Κ.Γ.ΤΑΣΟΥΛΗΣ-ΝΙΚ.ΠΟΛΥΚΑΝΔΡΙΩΤΗΣ copy.wav"}
 
-const exhibit_5  = {path: "images/5.png", id:6, titleGR: "Bentetta", titleEN:" ", date: "", descriptionGR: "", sound: ""}
+const exhibit_5  = {path: "images/5.png", id:6, titleGR: "Bentetta", titleEN:" ", date: "", descriptionGR: "", sound: "ΑΕΚ Ποντιακός.mp3"}
 const exhibit_6  = {path: "images/6.png", id:7, titleGR: "lilly", titleEN:" ", date: "", descriptionGR: "", sound: ""}
 const exhibit_7  = {path: "images/7.png", id:7, titleGR: "melcophone", titleEN:" ", date: "", descriptionGR: "", sound: ""}
 const exhibit_8  = {path: "images/8.png", id:7, titleGR: "mimis", titleEN:" ", date: "", descriptionGR: "", sound: ""}
